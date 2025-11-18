9b9399e3d8d354013154f2da8cfded9baf4ed0cf
Shion <seoy1108@gmail.com>
2025-11-18 11:40:23 +0900

    test: Button 컴포넌트 생성 및 userEvent 테스트

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,16 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Button } from './Button';
+
+test('버튼을 클릭하면 onClick 함수가 호출되는지 확인', () => {
+  // onClick props에 들어갈 함수를 모킹한다.
+  const handleClick = jest.fn();
+  // 모킹한 함수를 onClick props에 전달하여 버튼 렌더링
+  render(<Button onClick={handleClick}>버튼내용</Button>);
+
+  // 버튼을 클릭
+  const button = screen.getByText('버튼내용');
+  fireEvent.click(button);
+
+  // 버튼 클릭 시 모킹한 함수가 1번 호출되었는지 확인
+  expect(handleClick).toHaveBeenCalledTimes(1);
+});
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+export function Button({
+  children,
+  onClick,
+  disabled,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+}) {
+  return (
+    <button
+      className="bg-blue-400 px-4 py-2 rounded-md text-white cursor-pointer"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
