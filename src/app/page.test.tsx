d64223070544cdd00270df00594f6042f99c1680
Shion <seoy1108@gmail.com>
2025-11-19 10:14:27 +0900

    feat: 테스트 코드 에러 로직 추가

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Home from './page';
+import { server } from '@/mocks/node';
+import { http, HttpResponse } from 'msw';
 
 describe('MSW 모킹 테스트', () => {
   test('MSW 상세 데이터 모킹', async () => {
@@ -9,5 +11,39 @@ describe('MSW 모킹 테스트', () => {
 
     // MSW에서 설정한 결과값이 화면에 잘 나오는지 확인
     expect(postItem).toBeInTheDocument();
+  });
+
+  test('네트워크 에러 발생 시 모킹 테스트', async () => {
+    server.use(
+      http.get('http://localhost:4000/posts/1', () => {
+        return HttpResponse.error();
+      })
+    );
+
+    render(<Home />);
+
+    const errorMessage =
+      await screen.findByText('데이터를 불러오는데 실패했습니다.');
+
+    // 에러 메시지가 화면에 잘 나오는지 확인
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  test('서버 에러 시 모킹 테스트', async () => {
+    server.use(
+      http.get('http://localhost:4000/posts/1', () => {
+        // 백엔드에서 반환하는 값은 없고 (null)
+        // 상태 코드가 500으로 반환 -> catch 블록으로 이동
+        return HttpResponse.json(null, { status: 500 });
+      })
+    );
+
+    render(<Home />);
+
+    const errorMessage =
+      await screen.findByText('데이터를 불러오는데 실패했습니다.');
+
+    // 에러 메시지가 화면에 잘 나오는지 확인
+    expect(errorMessage).toBeInTheDocument();
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,16 @@ import type { Post } from '@/mocks/handlers/posts';
 
 export default function Home() {
   const [data, setData] = useState<Post | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get('http://localhost:4000/posts/1');
-      setData(response.data);
+      try {
+        const response = await axios.get('http://localhost:4000/posts/1');
+        setData(response.data);
+      } catch {
+        setError('데이터를 불러오는데 실패했습니다.');
+      }
     };
     fetchData();
   }, []);
@@ -25,6 +30,7 @@ export default function Home() {
           <p>{data.body}</p>
         </li>
       )}
+      {error && <p className="text-red-500">{error}</p>}
     </ul>
   );
 }
