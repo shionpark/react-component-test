ad9cb32d00d54d003bbf4e87b94fbf70a00a2132
Shion <seoy1108@gmail.com>
2025-11-19 11:05:53 +0900

    feat: 홈페이지 로드 테스트 추가 및 불필요한 데이터 로딩 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,3 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import type { Post } from '@/mocks/handlers/posts';
-
 export default function Home() {
-  const [data, setData] = useState<Post | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:4000/posts/1');
-        setData(response.data);
-      } catch {
-        setError('데이터를 불러오는데 실패했습니다.');
-      }
-    };
-    fetchData();
-  }, []);
-
-  return (
-    <ul>
-      {data && (
-        <li key={data.id} className="border p-4">
-          <h3 className="font-bold">
-            {data.id}: {data.title}
-          </h3>
-          <p>{data.body}</p>
-        </li>
-      )}
-      {error && <p className="text-red-500">{error}</p>}
-    </ul>
-  );
+  return <h1>게시글 목록</h1>;
 }
